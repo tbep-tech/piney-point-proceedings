1fdeb7359147c70f51f2107b3c3ee2ac12c071a8
fawda123 <mbafs2012@gmail.com>
2022-05-12 15:57:39 -0400

    added demographics table

--- R/tables.R
+++ R/tables.R
@@ -1,1 +1,37 @@
 library(tidyverse)
+library(lubridate)
+library(here)
+
+data(dashdem)
+
+# dashboard user demographics ---------------------------------------------
+
+grps <- unique(dashdem$Group)
+
+demtab <- NULL
+for(grp in grps){
+  
+  tmp <- dashdem %>%
+    filter(Group == grp) %>% 
+    mutate(
+      perc = round(100 * Users / sum(Users), 1), 
+      perc = paste0('(', perc, ')')
+    ) %>% 
+    arrange(-Users) %>% 
+    unite('Users', Users, perc, sep = ' ') %>% 
+    slice(1:10) %>% 
+    select(-Group)
+  
+  tot <- sum(dashdem$Group %in% grp)
+  nms <- paste0(grp, ' (', tot, ')')
+  
+  tmp <- tmp %>% 
+    rename(
+      !!nms := Name
+    )
+
+  demtab <- bind_cols(demtab, tmp, .name_repair = 'minimal') 
+  
+}
+
+save(demtab, file = here('data/demtab.RData'))  
