b19aabd0e095eb1c7cd296b0ec424aa00f051bcc
fawda123 <mbafs2012@gmail.com>
2022-05-13 13:18:20 -0400

    fixing analytics date range

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -10,10 +10,12 @@ dashusr <- read.csv(here('data/dashusrraw.csv')) %>%
     `Search engine` = Organic.Search,
     `Social media` = Organic.Social, 
     `Website referral` = Referral
-  )
+  ) %>% 
+  filter(Date <= ymd('2021-10-01'))
 
 save(dashusr, file = here('data/dashusr.RData'))
 
+# May 13 2021 to Oct 1 2021
 dashdem <- read.csv(here('data/dashdemraw.csv'))
 
 save(dashdem, file = here('data/dashdem.RData'))
