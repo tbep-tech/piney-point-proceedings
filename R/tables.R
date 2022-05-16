16eec770b581f96846082dcde64c52747676148e
fawda123 <mbafs2012@gmail.com>
2022-05-16 10:28:19 -0400

    added sampling effort table

--- R/tables.R
+++ R/tables.R
@@ -2,7 +2,38 @@ library(tidyverse)
 library(lubridate)
 library(here)
 
+data(rsphydat)
+data(rswqdat)
+data(rstrndat)
 data(dashdem)
+
+# sampling effort by month and type ---------------------------------------
+
+efftab <- list('Algae' = rsphydat, 'Water Quality' = rswqdat, 'Seagrass/macroalgae' = rstrndat) %>% 
+  enframe %>% 
+  unnest('value') %>% 
+  select(name, date, station) %>% 
+  unique %>% 
+  filter(date < ymd('2021-10-01')  & date >= ymd('2021-04-01')) %>% 
+  mutate(
+    Month = month(date, label = TRUE), 
+    name = factor(name, levels = c('Water Quality', 'Algae', 'Seagrass/macroalgae'))
+  ) %>% 
+  group_by(name, Month) %>% 
+  summarise(
+    cnt = length(unique(station)),
+    .groups = 'drop'
+  ) %>% 
+  group_by(name) %>% 
+  mutate(
+    percent = round(100 * cnt / sum(cnt), 1), 
+    percent = paste0('(', percent, ')')
+  ) %>% 
+  ungroup() %>% 
+  unite('cnt', cnt, percent, sep = ' ') %>% 
+  pivot_wider(names_from = 'name', values_from = 'cnt', values_fill = '-')
+
+save(efftab, file = here('data/efftab.RData'))
 
 # dashboard user demographics ---------------------------------------------
 
